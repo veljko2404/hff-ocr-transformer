7808885b854095f39354b6aa717532febc1e4537
Veljko Petkovic <38298277+veljko2404@users.noreply.github.com>
2026-03-15 05:03:27 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import torch
+
+N_IMAGES = 50_000 # total number of images to generate
+
+IMG_W, IMG_H = 256, 64 # images width and height
+
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
+
+MIN_LEN = 2 # minimum text length
+
+MAX_LEN = 20 # maximum text length
+
+ALLOWED = set(ALPHABET) # set of valid characters
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
